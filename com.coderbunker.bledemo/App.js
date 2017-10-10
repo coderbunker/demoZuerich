f9311c483dd33107e7360dfeca42004061c4cab4
PT\Pascal <pascal.tschuemperlin@hotmail.ch>
2017-10-10 17:53:09 +0800

    Added ble scanner with button. not working atm.

--- com.coderbunker.bledemo/App.js
+++ com.coderbunker.bledemo/App.js
@@ -1,42 +1,83 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, AppState, Platform, PermissionsAndroid, TouchableHighlight } from 'react-native';
 import BleManager from 'react-native-ble-manager';
 
 export default class App extends React.Component {
     constructor() {
 
         super();
-        BleManager.start()
-        .then(() => {
-            // Success code
-            console.log('Module initialized');
+
+        this.state = {
+            scanning: false,
+            peripherals: new Map(),
+            appState: ''
+        }
+
+        this.handleAppStateChange = this.handleAppStateChange.bind(this);
+    }
+
+    componentDidMount() {
+        AppState.addEventListener('change', this.handleAppStateChange);
+
+        BleManager.start({ showAlert: false });
+
+        if (Platform.OS === 'android' && Platform.Version >= 23) {
+            PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION).then((result) => {
+                if (result) {
+                    console.log("Permission is OK");
+                } else {
+                    PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION).then((result) => {
+                        if (result) {
+                            console.log("User accept");
+                        } else {
+                            console.log("User refuse");
+                        }
+                    });
+                }
             });
-}
+        }
+    }
+
+    handleAppStateChange(nextAppState) {
+        if (this.state.appState.match(/inactive|background/) && nextAppState === 'active') {
+            console.log('App has come to the foreground!')
+            BleManager.getConnectedPeripherals([]).then((peripheralsArray) => {
+                console.log('Connected peripherals: ' + peripheralsArray.length);
+            });
+        }
+        this.setState({ appState: nextAppState });
+    }
+
     startScan() {
         if (!this.state.scanning) {
             BleManager.scan([], 3, true).then((results) => {
                 console.log('Scanning...');
                 this.setState({ scanning: true });
+            }).catch(function (error) {
+                console.log('There has been a problem with your fetch operation: ' + error.message);
+                throw error;
             });
         }
     }
     render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Test Message</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-        <Text>Scan Bluetooth ({this.state.scanning ? 'on' : 'off'})</Text>
-        </View>
-    );
-  }
+        return (
+            <View style={styles.container}>
+            <TouchableHighlight style={{ marginTop: 40, margin: 20, padding: 20, backgroundColor: '#ccc' }} onPress={() => this.startScan()}>
+                <Text>Scan Bluetooth ({this.state.scanning ? 'on' : 'off'})</Text>
+            </TouchableHighlight>
+                <Text>Open up App.js to start working on your app!</Text>
+                <Text>Test Message</Text>
+                <Text>Shake your phone to open the developer menu.</Text>
+                </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
