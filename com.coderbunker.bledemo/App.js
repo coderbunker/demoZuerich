1674b777914cb184c76fb084ad7f529d33c97018
PT\Pascal <pascal.tschuemperlin@hotmail.ch>
2017-10-13 17:21:54 +0800

    added discover peripheral event handler and get discovered peripherals. Test deivce doens't find anything for some reason though.

--- com.coderbunker.bledemo/App.js
+++ com.coderbunker.bledemo/App.js
@@ -12,7 +12,9 @@ import {
     NativeModules,
 } from 'react-native';
 import BleManager from 'react-native-ble-manager';
-import Permissions from 'react-native-permissions';
+
+const BleManagerModule = NativeModules.BleManager;
+const bleManagerEmitter = new NativeEventEmitter(BleManagerModule);
 
 export default class App extends React.Component {
 
@@ -23,10 +25,14 @@ export default class App extends React.Component {
         this.state = {
             scanning: false,
             peripherals: new Map(),
-            appState: ''
+            appState: '',
+            discoveries: 99
         }
 
         this.handleAppStateChange = this.handleAppStateChange.bind(this);
+        this.handleDiscoverPeripheral = this.handleDiscoverPeripheral.bind(this);
+        this.handleStopScan = this.handleStopScan.bind(this);
+
     }
 
     componentDidMount() {
@@ -36,7 +42,28 @@ export default class App extends React.Component {
             console.log('BLE Module initialized');
         }).catch(function (e) {
             console.log("ERROR: Couldn't start BleManager: " + e)
-        });
+            });
+
+        if (Platform.OS = 'android') {
+            BleManager.enableBluetooth()
+                .then(() => {
+                    // Success code
+                    console.log('The bluetooth is already enabled or the user confirm');
+                })
+                .catch((error) => {
+                    // Failure code
+                    console.log('The user refuse to enable bluetooth');
+                });
+        }
+
+        this.handlerdiscover = bleManagerEmitter.addListener('BleManagerDiscoverPeripheral', this.handleDiscoverPeripheral);
+        this.handlerstop = bleManagerEmitter.addListener('BleManagerStopScan', this.handleStopScan);
+    }
+
+    componentWillUnmount()
+    {
+        this.handlerdiscover.remove();
+        this.handlerstop.remove();
     }
 
     handleAppStateChange(nextAppState) {
@@ -51,35 +78,55 @@ export default class App extends React.Component {
         this.setState({ appState: nextAppState });
     }
 
-    toggleScan() {
+    handleDiscoverPeripheral(peripheral)
+    {
+        console.log('is in Listener' + peripheral)
+        var peripherals = this.state.peripherals;
+        if (!peripherals.has(peripheral.id)) {
+            console.log('Got ble peripheral', peripheral);
+            peripherals.set(peripheral.id, peripheral);
+            this.setState({ peripherals })
+        }
+    }
+
+    handleStopScan() {
+        console.log('Scan is stopped');
+        this.setState({ scanning: false });
+    }
+
+    startScan() {
         if (!this.state.scanning) {
-            BleManager.scan([], 3, true).then((results) => {
+            BleManager.scan([], 10, true).then((results) => {
                 console.log('Scanning...');
                 this.setState({ scanning: true });
             }).catch(function (error) {
                 console.log('ERROR: There has been a problem with starting the scan: ' + error.message);
             });
         }
-        else {
-            BleManager.stopScan()
-                .then(() => {
-                    // Success code
-                    console.log('Scan stopped');
-                    this.setState({ scanning: false });
-                }).catch(function (error) {
-                    console.log('ERROR: There has been a problem with starting the scan: ' + error.message);
-                });
-        }
     }
+    getDiscoveries()
+    {
+        BleManager.getDiscoveredPeripherals([])
+            .then((peripheralsArray) => {
+                // Success code
+                console.log('Discovered peripherals: ' + peripheralsArray.length);
+                this.setState({ discoveries: peripheralsArray.length });
+            }).catch(function (error) {
+                console.log('ERROR: There has been a problem with accesing the discovered peripherals: ' + error.message);
+            });
+    }
+
   render() {
     return (
         <View style={styles.container}>
-            <TouchableHighlight style={{ marginTop: 40, margin: 20, padding: 20, backgroundColor: '#ccc' }} onPress={() => this.toggleScan()}>
-                <Text>Scan Bluetooth ({this.state.scanning ? 'on' : 'off'})</Text>
+            <TouchableHighlight style={{ marginTop: 40, margin: 20, padding: 20, backgroundColor: '#ccc' }} onPress={() => this.startScan()}>
+                <Text>Scan Bluetooth</Text>
             </TouchableHighlight>
-            <Text>Open up App.js to start working on your app!</Text>
-            <Text>Test Message</Text>
-            <Text>Shake your phone to open the developer menu.</Text>
+            <Text>Scanning {this.state.scanning ? 'on' : 'off'} </Text>
+            <TouchableHighlight style={{ marginTop: 40, margin: 20, padding: 20, backgroundColor: '#ccc' }} onPress={() => this.getDiscoveries()}>
+                <Text>Get Found Devices</Text>
+            </TouchableHighlight>
+            <Text>Devices discovered {this.state.discoveries} </Text>
         </View>
     );
   }
