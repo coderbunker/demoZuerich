3038238277da6e3fa1fed999a7e97401b90f9511
PT\Pascal <pascal.tschuemperlin@hotmail.ch>
2017-10-17 16:49:55 +0800

    recreate project. now scanning finds peripherals.

--- com.coderbunker.bledemo/App.js
+++ com.coderbunker.bledemo/App.js
@@ -32,6 +32,7 @@ export default class App extends React.Component {
         this.handleAppStateChange = this.handleAppStateChange.bind(this);
         this.handleDiscoverPeripheral = this.handleDiscoverPeripheral.bind(this);
         this.handleStopScan = this.handleStopScan.bind(this);
+
     }
 
     componentDidMount() {
@@ -70,10 +71,6 @@ export default class App extends React.Component {
             console.log('App has come to the foreground!')
             BleManager.getConnectedPeripherals([]).then((peripheralsArray) => {
                 console.log('Connected peripherals: ' + peripheralsArray.length);
-                if (peripheralsArray.length > 0)
-                {
-                    console.log(peripheralsArray);
-                }
             }).catch(function (e) {
                 console.log(e); // "oh, no!"
             });
@@ -107,7 +104,6 @@ export default class App extends React.Component {
             });
         }
     }
-
     getDiscoveries()
     {
         BleManager.getDiscoveredPeripherals([])
